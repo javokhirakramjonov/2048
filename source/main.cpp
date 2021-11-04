9885087aca2fcab9ac407017a14c02941fab684f
Javokhir Akramjonov <javokhirakromjonov@gmail.com>
2021-11-04 06:43:14 +0300

    replaces files

--- source/main.cpp
+++ source/main.cpp
@@ -1,23 +1,11 @@
-#include "Game2048.h"
+#include "game1.h"
 
 #include <QApplication>
-#include <QLocale>
-#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "2048_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
-    Game2048 w;
+    GAME1 w;
     w.show();
     return a.exec();
 }
